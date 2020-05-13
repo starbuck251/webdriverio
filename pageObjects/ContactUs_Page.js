f4c381836ead37db3486a828715df2cc7e4eadc7
Zoe Buck <zoeb@immjsystems.com>
2020-05-13 11:27:23 +0100

    POM updates

--- pageObjects/ContactUs_Page.js
+++ pageObjects/ContactUs_Page.js
@@ -1,33 +1,50 @@
 class ContactUs_Page {
     // ---- start of creation of page object model params----
     //Page elements
-    get firstName() {
-        return $("[name='first_name']");
-    }
-
-    get lastName() {
-        return $("[name='last_name']");
-    }
-
-    get emailAddress() {
-        return $("[name='email']");
-    }
-
-    get comments() {
-        return $("[name='message']");
-    }
-
-    get submitButton() {
-        return $("[type='submit']");
-    }
-
-    get successfulSubmissionText() {
-        return $("#contact_reply h1");
-    }
-
-    get failedText() {
-        return $("body");
-    }
-
+    get firstName() { return $("[name='first_name']"); }
+    get lastName() {return $("[name='last_name']");}
+    get emailAddress() {return $("[name='email']");}
+    get comments() {return $("[name='message']");}
+    get submitButton() {return $("[type='submit']");}
+    get successfulSubmissionText() {return $("#contact_reply h1");}
+    get failedText() {return $("body");}
 }
+
+// functions
+function clickSubmitButton() {
+    return this.submitButton.click();
+}
+
+submitAllInformationViaContactUsForm(firstName, lastName, emailAddress, comments){
+    if (firstName) {
+        this.firstName.setValue(firstName);
+    }
+    if (lastName) {
+        this.lastName.setValue(lastName);
+    }
+    if (emailAddress) {
+        this.emailAddress.setValue(emailAddress);
+    }
+    if (comments) {
+        this.comments.setValue(comments);
+    }
+}
+
+function confirmSuccessfulSubmission() {
+    var validateSubmissionHeader = browser.waitUntil(function () {
+        return this.successfulSubmissionText.getText() == 'Thank You for your Message!'
+        console.log('8');
+    }, 3000)
+    console.log('9');
+    expect(validateSubmissionHeader, 'Successful submission message does not exist!').to.be.true;
+}
+
+function confirmFailingSubmission() {
+    var validateSubmissionHeader = browser.waitUntil(function () {
+        return this.failedText.getText() == 'Error: all fields are required'
+    }, 3000)
+    expect(this.failedText.getText()).to.include('Error: all fields are required')
+}
+
+
 module.exports = new ContactUs_Page();
--- tests/contactUsTest.js
+++ tests/contactUsTest.js
@@ -5,81 +5,40 @@ beforeEach(function () {
 })
 
 describe('Test the contact us form', function () {
-
-
-    //SETTING VALUES INTO EACH INPUT BOX OF CONTACT US PAGE
-    function setFirstName(firstName) {
-        return ContactUs_Page.firstName.setValue(firstName);
-    }
-    function setLastName(lastName) {
-        return ContactUs_Page.lastName.setValue(lastName);
-    }
-    function setEmailAddress(emailAddress) {
-        return ContactUs_Page.emailAddress.setValue(emailAddress);
-    }
-    function setComments(comments) {
-        return ContactUs_Page.comments.setValue(comments);
-    }
-    function clickSubmitButton() {
-        return ContactUs_Page.submitButton.click();
-    }
-
-    function confirmSuccessfulSubmission() {
-        var validateSubmissionHeader = browser.waitUntil(function () {
-            console.log('7');
-            return ContactUs_Page.successfulSubmissionText.getText == 'Thank You for your Message!'
-            console.log('8');
-        }, 3000)
-        console.log('9');
-        expect(validateSubmissionHeader, 'Successful submission message does not exist!').to.be.true;
-    }
-
-    function confirmFailingSubmission() {
-        var validateSubmissionHeader = browser.waitUntil(function () {
-            return ContactUs_Page.failedText.getText == 'Error: all fields are required'
-        }, 3000)
-        expect(ContactUs_Page.failedText.getText).to.include('Error: all fields are required')
-    }
-
+    
     //  -------------- tests-------------------
     it('should be able to submit a successful submission via contact us form', function (done) {
         setFirstName('Joe');
-        console.log('1');
-            setLastName('Bloggs');
-        console.log('2');
-            setEmailAddress('Joe.smith@test.com');
-        console.log('3');
-            setComments('message');
-        console.log('4');
-            clickSubmitButton();
-        console.log('5');
-            confirmSuccessfulSubmission();
-        console.log('6');
+        setLastName('Bloggs');
+        setEmailAddress('Joe.smith@test.com');
+        setComments('message');
+        clickSubmitButton();
+        confirmSuccessfulSubmission();
     });
 
 
-    // it('should not be able to submit as all fields required - message missing', function (done) {
-    //     setFirstName('Joe');
-    //     setLastName('Bloggs');
-    //     setEmailAddress('Joe.smith@test.com');
-    //     clickSubmitButton();
-    //     confirmFailingSubmission();
-    // });
+    it('should not be able to submit as all fields required - message missing', function (done) {
+        setFirstName('Joe');
+        setLastName('Bloggs');
+        setEmailAddress('Joe.smith@test.com');
+        clickSubmitButton();
+        confirmFailingSubmission();
+    });
 
-    // it('should not be able to submit as all fields required - first name missing', function (done) {
-    //     setLastName('Bloggs');
-    //     setEmailAddress('Joe.smith@test.com');
-    //     setComments('hello world');
-    //     clickSubmitButton();
-    //     confirmFailingSubmission();
-    // });
+    it('should not be able to submit as all fields required - first name missing', function (done) {
+        setLastName('Bloggs');
+        setEmailAddress('Joe.smith@test.com');
+        setComments('hello world');
+        clickSubmitButton();
+        confirmFailingSubmission();
+    });
 
-    // it('should not be able to submit as all fields required - last name missing', function (done) {
-    //     setFirstName('Joe');
-    //     setEmailAddress('Joe.smith@test.com');
-    //     setComments('hello world');
-    //     clickSubmitButton();
-    //     confirmFailingSubmission();
-    // });
+    it('should not be able to submit as all fields required - last name missing', function (done) {
+        setFirstName('Joe');
+        setEmailAddress('Joe.smith@test.com');
+        setComments('hello world');
+        clickSubmitButton();
+        confirmFailingSubmission();
+    });
 
 });
